f0b03bc06b49e94c08eb0b915e5b27cd743e472e
NotAwful <not.awful.git@gmail.com>
2017-01-17 19:16:29 -0800

    README Consolidation and Server update
    Server: Added If statements for CSV checks, pruned my own comments now
    what I know how my script works, cleaned up comment text

--- BaselineHealth-Server.ps1
+++ BaselineHealth-Server.ps1
@@ -8,14 +8,12 @@ them in a write-only network share.
 
 $TargetList = Get-Content "TargetList.txt"
 $TargetList | ForEach-Object -begin {
-    <# This runs before checking any of the targets, so use it to do variables to be used throughout the whole thing #>
     $checksXML = ("CsPhysicallyInstalledMemory","OsVersion","OsBuildNumber")
     $checksCSV = ("AVProductState,HDDSerialNo")
     [string]$date = Get-Date -UFormat %Y-%m-%d
 } -process {
     $CurrentTarget = $_
     $checksXML | ForEach-Object -begin {
-        #Start XML "Begin" Block
         $latestXML = (Import-CliXml \\WOShare\$CurrentTarget.$date.xml)
         if ((TestPath "\\WOShare\$CurrentTarget.*.baseline.xml") -eq $true) {
             $baselineXML = Import-CliXml "\\WOShare\$CurrentTarget.*.baseline.xml"
@@ -23,7 +21,6 @@ $TargetList | ForEach-Object -begin {
             #Rename the current XML to be the baseline XML, then set $baselineXML as normal
         }
     } -process {
-        #Start XML "Process" Block
         if ($baselineXML.$_ -ne $latestXML.$_) {
             $changeTemp = New-Object System.Object
             $changeTemp | Add-Member -MemberType NoteProperty -Name "Date" -Value "$date"
@@ -31,17 +28,13 @@ $TargetList | ForEach-Object -begin {
             $changeTemp | Add-Member -MemberType NoteProperty -Name "Old Value" -Value "$baselineXML.$_"
             $changeTemp | Add-Member -MemberType NoteProperty -Name "New Value" -Value "$latestXML.$_"
             $changeTemp | Export-Csv $CurrentTarget.changelog.csv -append
-        } #End if statement
+        }
     } -end {
-        #Start XML "End" Block
         if ($baselineXML.CsPhysicallyInstalledMemory -ne $latestXML.CsPhysicallyInstalledMemory) {
-            <# If baseline's installed memory is not equal to the latest installed memory then write an event to the...
-                Event Log, triggering a Scheduled Task to send an email to IT that $CurrentTarget's installed RAM has changed.
-            #>
-        } #End If statement
-    } #End XML "End" block
+            <# Write event to event log, triggering scheduled task to alert IT that RAM has changed #>
+        }
+    } #End XML block
     $checkCSV | ForEach-Object -begin {
-        # Start CSV "Begin" Block
         $lastestCSV = (Import-CSV \\WOShare\$CurrentTarget.$date.CSV)
         if ((TestPath "\\WOShare\$CurrentTarget.*.baseline.csv") -eq $true) {
             $baselineCSV = "\\WOShare\$CurrentTarget.*.baseline.csv"
@@ -49,7 +42,6 @@ $TargetList | ForEach-Object -begin {
             #Rename the current CSV to be the baseline CSV, then set $baselineCSV as normal
         }
     } -process {
-        # Start CSV "Process" Block
         if ($baselineCSV.$_ -ne $latestCSV.$_) {
             $changeTemp = New-Object System.Object
             $changeTemp | Add-Member -MemberType NoteProperty -Name "Date" -Value "$date"
@@ -57,11 +49,15 @@ $TargetList | ForEach-Object -begin {
             $changeTemp | Add-Member -MemberType NoteProperty -Name "Old Value" -Value "$baselineCSV.$_"
             $changeTemp | Add-Member -MemberType NoteProperty -Name "New Value" -Value "$latestCSV.$_"
             $changeTemp | Export-Csv $CurrentTarget.changelog.csv -append
-        } #End if statement
+        }
     } -end {
-        #Start CSV "End" Block
-        # Alerts/Remediation for XML
-    } #End CSV "End" Block
+        if ($baselineCSV.AVProductState -ne $latestCSV.AVProductState) {
+            <# Write event to event log, triggering scheduled task to alert IT that active AVs have changed #>
+        }
+        if ($baselineCSV.HDDSerialNo -ne $latestCSV.HDDSerialNo) {
+            <# Write event to event log, triggering scheduled task to alert IT that installed disks have changed #>
+        }
+    } #End CSV Block
 } -end {
     <## Reporting ##>
 }
--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# powershell-baseline
-
-
 # SUMMARY
 
 A set of Powershell v5.1 scripts that retrieves baseline information about host machines on a network and maintains a changelog of those machines over time.
@@ -11,6 +8,37 @@ A set of Powershell v5.1 scripts that retrieves baseline information about host 
 The host script stores information in a write-only non-enumeration network share. The server-side script requires read/write access to the same share.
 
 
-# ABOUT
+# HOST
+STRUCTURE
+Declare Variables
+XML
+	Get-ComputerInfo & export to WOShare
+CSV
+	Create Object to store values
+	Add Values to Object
+	Object | Export-Csv (-append) to WOShare
 
-Each script will have a text file associated with it that has information about the script. If it requires administrative rights, etc.
+# SERVER
+STRUCTURE
+get targets
+foreach-Object vs targets {
+	Begin: List Checks
+		Process: foreach-Object vs XML {
+			begin: does baseline exist?
+			process: do checks, update changelog
+			end: generate alerts if required, or remediation if you're good
+		} Close vs XML
+		Process: foreach-Object vs CSV {
+			begin: does baseline exist?
+			process: do checks, update changelog
+			end: generate alerts if required, or remediation if you're good
+		} Close vs CSV
+	End: Report that baselining checks are done.
+} Close vs targets
+
+REQUIREMENTS
+Get-ComputerInfo was implemented in Powershell 5.1, so targets need that for this to even be conceptually sound.
+Need a write-only network share for logs to be placed.
+
+# TO-DO
+Server: Create Event Logic, Baseline-setting (need to work out logic for automating this), 
