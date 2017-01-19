f3a525972d1ec579f85eeefbe680c24c20cdf954
NotAwful <not.awful.git@gmail.com>
2017-01-18 16:51:06 -0800

    Moved to separate repo
    Moved proof of concept scripts to their own repository.

--- Proofs/Object Creation.ps1
+++
@@ -1,9 +0,0 @@
-﻿<# Object Creation Streamline Proof #>
-[string]$date = Get-Date -UFormat %Y-%m-%d
-$changeTemp = New-Object PSObject
-$A = [ordered]@{Date="$date";Item="";OldValue="";NewValue=""}
-$changeTemp | Add-Member -NotePropertyMembers $A
-
-$changeTemp | Get-Member
-
-$changeTemp.date
